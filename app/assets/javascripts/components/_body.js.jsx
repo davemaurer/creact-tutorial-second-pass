466ae41437e60efdccd0cd56d2160f4bd1f5b5e6
David Maurer <davemaurermail@gmail.com>
2015-12-24 08:44:18 -0700

    new skill rendering on refresh

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,8 +1,23 @@
 var Body = React.createClass({
+  getInitialState() {
+    return {skills: []}
+  },
+
+  componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => {
+      this.setState({skills: response})
+    });
+  },
+
+  handleSubmit(skill) {
+    console.log(skill);
+  },
+
   render() {
     return (
       <div>
-        <AllSkills />
+        <NewSkill handleSubmit={this.handleSubmit} />
+        <AllSkills skills={this.state.skills}/>
       </div>
     )
   }
